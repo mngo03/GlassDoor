b62d8e5de537575f4f17b948b5c890bea6438634
MawRojas <m.aur@live.com.mx>
2018-09-10 10:16:13 -0400

    Added Bootstrap and routing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,26 @@ import { RegistrationComponent } from './registration/registration.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { JobsRecruitingComponent } from './jobs-recruiting/jobs-recruiting.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { RoutingComponentComponent } from './routing-component/routing-component.component';
+
+//Add your routes here
+const routes: Routes = [
+  {path: 'company_details', component: CompanyDetailsComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     CompanyDetailsComponent,
     RegistrationComponent,
     ReviewsComponent,
-    JobsRecruitingComponent
+    JobsRecruitingComponent,
+    RoutingComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
